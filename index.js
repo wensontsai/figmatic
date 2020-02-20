cfc4d08b33d8e137c3f1a5ce0dd382d220e51399
wensontsai <wensontsai@gmail.com>
2020-02-19 20:39:49 -0500

    changing to a calculator

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-module.exports = function(width, height) {
-    return width * height;
+var Calculator = function () { };
+
+Calculator.prototype.add = function (a, b) {
+    return a + b;
 }
+
+Calculator.prototype.subtract = function (a, b) {
+    return a - b;
+}
+
+Calculator.prototype.multiply = function (a, b) {
+    return a * b;
+}
+
+Calculator.prototype.divide = function (a, b) {
+    return a / b;
+}
+
+module.exports = new Calculator();
